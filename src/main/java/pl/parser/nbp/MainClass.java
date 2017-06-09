5c4b2eec789f32e99937c601da0ed3f45b3cb959
Michał Piasecki 203965 <203965.edu.p.lodz.pl>
2017-06-09 19:16:17 +0200

    Dodano pliki projektu Maven

---
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -0,0 +1,12 @@
+package pl.parser.nbp;
+
+/**
+ * @author Michał Piasecki
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
